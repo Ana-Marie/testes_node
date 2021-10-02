a3d139d5800b72e183d5204a1375afd6beb6afaf
Pedro <pedro_rique17@hotmail.com>
2021-10-02 15:37:31 -0300

    teste 1

---
+++ random_number/test/generate.test.js
@@ -0,0 +1,21 @@
+const gerarNumeroAleatorio = require('../lib/generate');
+
+/* TESTES
+
+1. início do intervalo é um valor negativo;
+2. o fim do intervalo é um valor negativo;
+3. o início do intervalo é igual ao fim do intervalo;
+4. um intervalo válido [200, 3000] é fornecido 
+
+*/
+
+let inicio = 100;
+let fim = 500;
+
+describe('Generate', () => {
+
+    test('Teste 1', () =>{
+        expect(inicio).toBeGreaterThan(0); // se for maior que zero, passa no teste
+    });
+
+});
